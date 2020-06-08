38da3494dde9b7f50a7a7a3b1657f6e3a42b1b3f
Nguyen Ngoc Khanh <nguyenngoc.khanh@shopee.com>
2020-06-09 00:03:07 +0800

    ...

--- heap_test.go
+++ heap_test.go
@@ -1,8 +1,10 @@
 package heap
 
 import (
+	"fmt"
 	"math/rand"
 	"testing"
+	"time"
 )
 
 type Int int
@@ -11,11 +13,41 @@ func (i Int) Less(j Node) bool {
 	return i < j.(Int)
 }
 
+func timer(function func()) {
+	t := time.Now()
+	function()
+	fmt.Printf("elapsed time: %v\n", time.Since(t))
+}
+
 func TestHeap(t *testing.T) {
-	h := New()
-	randRange := 100
-	numNodes := 100
+	numHeaps := 1000
+	numNodes := 1000
+	randRange := 1000
+	nodes := make([]Node, numNodes)
 	for i := 0; i < numNodes; i++ {
-		h.Push(Int(rand.Intn(randRange)))
+		nodes[i] = Int(rand.Intn(randRange))
 	}
+
+	timer(func() {
+		for t := 0; t < numHeaps; t++ {
+			h := New()
+			for _, node := range nodes {
+				h.Push(node)
+			}
+		}
+	})
+
+	nodess := make([][]Node, numHeaps)
+	for i := range nodess {
+		nodess[i] = make([]Node, numNodes)
+		for j := range nodess[i] {
+			nodess[i][j] = nodes[j]
+		}
+	}
+	timer(func() {
+		for t := 0; t < numHeaps; t++ {
+			h := FromArray(nodess[t])
+			_ = h
+		}
+	})
 }
